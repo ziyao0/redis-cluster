c21f3601dd5a7907c667ad49904cf47c80f8e0ca
DDAREWRWRE <1337891505@qq.com>
2020-02-11 16:36:59 +0800

    初始化信息

---
+++ src/main/java/com/zzy/rediscluster/controller/OrderController.java
@@ -0,0 +1,43 @@
+package com.zzy.rediscluster.controller;
+
+import com.zzy.rediscluster.lock.RedisLock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+/**
+ * @author:zzy
+ * @date: 2020/2/11
+ * @time: 12:43
+ */
+@RestController
+public class OrderController {
+    private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
+
+    @Autowired
+    private RedisLock redisLock;
+
+    /**
+     * 创建订单
+     * @param productId
+     * @return
+     */
+    @RequestMapping("createOrder")
+    public String deductStock(String productId) {
+        String result = null;
+        try {
+            result = redisLock.deductStock(Objects.requireNonNull(productId));
+            logger.info("下单成功！");
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("系统异常：" + e);
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/zzy/rediscluster/lock/RedisLock.java
@@ -0,0 +1,61 @@
+package com.zzy.rediscluster.lock;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author:zzy
+ * @date: 2020/2/11
+ * @time: 12:10
+ */
+@Service
+public class RedisLock {
+
+    private static final Logger logger = LoggerFactory.getLogger(RedisLock.class);
+
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    /**
+     * 库存操作
+     *
+     * @param lockKey
+     * @return
+     */
+    public String deductStock(String productId) {
+        String lockKey = productId + "LOCK";
+        String clientId = UUID.randomUUID().toString();
+        try {
+            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, clientId, 30, TimeUnit.SECONDS);
+            if (!result) {
+                return "FALSE";
+            }
+            int stock = Integer.parseInt(Objects.requireNonNull(stringRedisTemplate.opsForValue().get(productId)));
+            if (stock > 0) {
+                int resStock = stock - 1;
+                stringRedisTemplate.opsForValue().set(productId, resStock + "");
+                System.out.println("扣减库存成功,剩余库存" + resStock);
+                logger.info("扣减库存成功,剩余库存" + resStock);
+            } else {
+                logger.info("库存不足");
+                System.out.println("库存不足，扣减库存失败！");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("扣减库存失败：" + e);
+            return "FALSE";
+        } finally {
+            if (clientId.equals(stringRedisTemplate.opsForValue().get(lockKey))) {
+                stringRedisTemplate.delete(lockKey);
+            }
+        }
+        return "SUCCESS";
+    }
+}
