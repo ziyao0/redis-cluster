86d6cfd7e65e846f5971b9ccbcd06b14f3998b06
DDAREWRWRE <1337891505@qq.com>
2020-02-12 10:59:42 +0800

    Redisson实现锁续命

--- src/main/java/com/zzy/rediscluster/service/impl/OrderServiceImpl.java
+++ src/main/java/com/zzy/rediscluster/service/impl/OrderServiceImpl.java
@@ -1,6 +1,8 @@
 package com.zzy.rediscluster.service.impl;
 
 import com.zzy.rediscluster.service.OrderService;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +25,8 @@ public class OrderServiceImpl implements OrderService {
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
+    @Autowired
+    private Redisson redisson;
 
     /**
      * 库存操作
@@ -33,12 +37,17 @@ public class OrderServiceImpl implements OrderService {
     @Override
     public String createOrder(String productId) {
         String lockKey = productId + "LOCK";
-        String clientId = UUID.randomUUID().toString();
+//        String clientId = UUID.randomUUID().toString();
+
+        RLock redissonLock = redisson.getLock(lockKey);
         try {
-            Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, clientId, 30, TimeUnit.SECONDS);
+           /* Boolean result = stringRedisTemplate.opsForValue().setIfAbsent(lockKey, clientId, 30, TimeUnit.SECONDS);
             if (!result) {
                 return "FALSE";
-            }
+            }*/
+
+            //加锁  实现锁续命
+            redissonLock.lock();
             int stock = Integer.parseInt(Objects.requireNonNull(stringRedisTemplate.opsForValue().get(productId)));
             if (stock > 0) {
                 int resStock = stock - 1;
@@ -54,9 +63,11 @@ public class OrderServiceImpl implements OrderService {
             logger.error("扣减库存失败：" + e);
             return "FALSE";
         } finally {
-            if (clientId.equals(stringRedisTemplate.opsForValue().get(lockKey))) {
+            //释放锁
+            redissonLock.unlock();
+            /*if (clientId.equals(stringRedisTemplate.opsForValue().get(lockKey))) {
                 stringRedisTemplate.delete(lockKey);
-            }
+            }*/
         }
         return "SUCCESS";
     }
